9c11cdfb6d90b923fdbde7dbc1b7a7a4afe95e7d
Joao Caio <jocaio@uol.com.br>
2021-11-18 19:03:54 -0300

    Tratamentos dentro do array substituir, procurar, remover, etc.

---
+++ script.js
@@ -0,0 +1,118 @@
+let lista = ['ovo', 'farinha', 'corante', 'massa'];
+
+lista.sort();
+// função em oredem alfabetica
+lista.reverse();
+// inverte o array
+
+let res = lista;
+
+console.log(res);
+
+/*
+// oredenação do array
+let lista = ['ovo', 'farinha', 'corante', 'massa'];
+
+lista.sort();
+// função em oredem alfabetica
+lista.reverse();
+// inverte o array
+
+let res = lista;
+*/
+
+
+/*
+// concatenação de arrays
+let lista = ['ovo', 'farinha', 'corante', 'massa'];
+let lista2 = ['prato', 'liquidificador', 'forno'];
+
+let res = lista.concat(lista2);
+*/
+
+
+
+/*
+// remover itens do array .splice( 1ºqual item quero remover, 2º quantos itens quero remover)
+let lista = ['ovo', 'farinha', 'corante', 'massa'];
+
+lista.splice(1, 1);
+
+let res = lista;
+*/
+
+
+/*
+// usando a busca dentro do array, 'length' busca a quantidade e insere no ultimo item o liquidificado
+let lista = ['ovo', 'farinha', 'corante', 'massa'];
+
+lista[ lista.length] = 'liquidificador';
+
+let res = lista;
+*/
+
+
+/*
+// substitui o item que eu procuro no array [0]
+let lista = ['ovo', 'farinha', 'corante', 'massa'];
+
+lista[0] = 'ovos';
+
+let res = lista;
+*/
+
+/*
+// adiciona item por ultimo no array
+let lista = ['ovo', 'farinha', 'corante', 'massa'];
+
+lista.push('prato');
+
+let res = lista;
+*/
+
+
+/*
+// remove o primeiro item
+let lista = ['ovo', 'farinha', 'corante', 'massa'];
+
+lista.shift();
+
+
+let res = lista;
+*/
+
+
+/*
+// remove o ultimo item
+let lista = ['ovo', 'farinha', 'corante', 'massa'];
+
+lista.pop();
+
+
+let res = lista;
+*/
+
+
+/*
+// procura o item dentro do array
+let lista = ['ovo', 'farinha', 'corante', 'massa'];
+
+let res = lista.indexOf('corante');
+*/
+
+
+/*
+// separa todos os itens por: '-'
+let lista = ['ovo', 'farinha', 'corante', 'massa'];
+
+let res = lista.join('-');
+*/
+
+
+
+/*
+// tranforma os itens do array em string
+let lista = ['ovo', 'farinha', 'corante', 'massa'];
+
+let res = lista.toString();
+*/
